1a0cdbc26847091d3b050931ff3d024ecdd882c5
Yang <yangacohs@hotmail.com>
2019-05-07 16:02:17 +1000

    Modules updates

---
+++ Modules/Entities/UserProfilesAccess.swift
@@ -0,0 +1,9 @@
+//
+//  UserProfilesAccess.swift
+//  InfosafeCSI onMove
+//
+//  Created by Feng Liu on 7/5/19.
+//  Copyright © 2019 Chemical Safety International. All rights reserved.
+//
+
+import Foundation
---
+++ Modules/Entities/UserProfilesData.swift
@@ -0,0 +1,9 @@
+//
+//  UserProfiles.swift
+//  InfosafeCSI onMove
+//
+//  Created by Feng Liu on 7/5/19.
+//  Copyright © 2019 Chemical Safety International. All rights reserved.
+//
+
+import Foundation
