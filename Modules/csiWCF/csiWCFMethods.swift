a856b709380f646ecbdaf934ce8a980ecf3d21bd
Yang <yangacohs@hotmail.com>
2019-05-07 15:51:41 +1000

    WCF Module Update

---
+++ Modules/csiWCF/csiWCFMethods.swift
@@ -0,0 +1,28 @@
+//
+//  WCFMethods.swift
+//  InfosafeCSI onMove
+//
+//  Created by Releski Tan on 7/5/19.
+//  Copyright © 2019 Chemical Safety International. All rights reserved.
+//
+
+import Foundation
+
+
+func csiWCF_loginbyEami(email:String, password:String, deviceid:String, devicemac:String) -> String
+{
+    var retValues:String!
+    //WCF for LoginByEamil
+    
+    
+    
+    return csiWCF_ReturnValueFix(inValue: retValues)
+}
+
+func  csiWCF_GetSDSSearchResultsPageEx(inputData:String) -> String {
+    <#function body#>
+}
+
+func csiWCF_ReturnValueFix(inValue:String) -> String{
+
+}
