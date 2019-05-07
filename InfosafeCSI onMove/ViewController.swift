e2bdeecc6fa0969205d088ab40d46bfbe7a11d47
Releski <releski@hotmail.com>
2019-05-07 11:44:43 +1000

    Initial Commit

---
+++ InfosafeCSI onMove/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfosafeCSI onMove
+//
+//  Created by Feng Liu on 7/5/19.
+//  Copyright © 2019 Chemical Safety International. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InfosafeCSI onMoveTests/InfosafeCSI_onMoveTests.swift
@@ -0,0 +1,34 @@
+//
+//  InfosafeCSI_onMoveTests.swift
+//  InfosafeCSI onMoveTests
+//
+//  Created by Feng Liu on 7/5/19.
+//  Copyright © 2019 Chemical Safety International. All rights reserved.
+//
+
+import XCTest
+@testable import InfosafeCSI_onMove
+
+class InfosafeCSI_onMoveTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
