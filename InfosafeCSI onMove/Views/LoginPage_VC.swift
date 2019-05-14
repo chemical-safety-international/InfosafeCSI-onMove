3f09001ee9bbd9a254d00efc25c7448fd41a5673
Yang <yangacohs@hotmail.com>
2019-05-14 16:02:03 +1000

    Login spinner

--- InfosafeCSI onMove/Views/LoginPage_VC.swift
+++ InfosafeCSI onMove/Views/LoginPage_VC.swift
@@ -36,6 +36,7 @@ class LoginPage_VC: UIViewController {
         let email = userIDTextField.text!
         let password = passwordTextField.text!
         
+        self.showSpinner(onView: self.view)
 
         csiWCF_VM().callLogin(email: email, password: password)
         
@@ -47,6 +48,7 @@ class LoginPage_VC: UIViewController {
 //            let ac = UIAlertController(title: "Login success", message: "Welcome", preferredStyle: .alert)
 //            ac.addAction(UIAlertAction(title: "OK", style:  .default))
 //            self.present(ac, animated: true)
+            self.removeSpinner()
             
             let loginJump = self.storyboard?.instantiateViewController(withIdentifier: "SearchPage") as? SearchPage_VC
             self.navigationController?.pushViewController(loginJump!, animated: true)
@@ -69,3 +71,27 @@ class LoginPage_VC: UIViewController {
 
 }
 
+var vSpinner : UIView?
+
+extension UIViewController {
+    func showSpinner(onView: UIView){
+        let spinnerView = UIView.init(frame: onView.bounds)
+        spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        let ai = UIActivityIndicatorView.init(style: .whiteLarge)
+        ai.startAnimating()
+        ai.center = spinnerView.center
+        
+        DispatchQueue.main.async {
+            spinnerView.addSubview(ai)
+            onView.addSubview(spinnerView)
+        }
+        vSpinner = spinnerView
+    }
+    
+    func removeSpinner(){
+        DispatchQueue.main.async {
+            vSpinner?.removeFromSuperview()
+            vSpinner = nil
+        }
+    }
+}
